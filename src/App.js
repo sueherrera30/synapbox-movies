42e0d6cf35a62a66ad2c217ddacc2f631c08d64f
sue <sue@MacBook-Pro-de-sue.local>
2019-12-29 00:06:19 -0600

    adding MUI theme into global app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import './App.css';
+import Home from '../src/containers/Home';
+
+const client = new ApolloClient({
+  uri: 'https://us1.prisma.sh/john-a-agudelo-e911b8/johnaagudelodb/dev'
+})
 
 function App() {
   return (
-    <div className="App">
-       Synapbox movies
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Home />
+      </div>
+    </ApolloProvider>
   );
 }
 
